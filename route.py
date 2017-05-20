1b56721c6f796345d8ed53bcae93aedadd686e91
Devansh <devanshs212@gmail.com>
2017-05-21 01:18:50 +0530

    added comments

--- route.py
+++ route.py
@@ -3,13 +3,18 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def check():
    return 'Server Working'
 
+#defining the API here
 @app.route('/getTweets')
 def getRetweets():
+
+	#create an instance of Tweets class
 	t = Tweets()
-	tweet_list = t.getRetweets()
+	tweet_list = t.getTweets()
+
+	#return the list of tweets
 	return jsonify(results=tweet_list)
 
 if __name__ == '__main__':
--- tweet.py
+++ tweet.py
@@ -3,23 +3,32 @@ import json
 import config
 
 class Tweets:
+	
+	#accessing the authorisation tokens from config.py
 	ckey = config.auth['ckey']
 	csecret = config.auth['csecret']
 	atoken = config.auth['atoken']
 	asecret = config.auth['asecret']
 
-	def getRetweets(self):
+	#method defined to extract the tweets
+	def getTweets(self):
+
 		tweet_list = []
-		auth = tweepy.OAuthHandler(self.ckey, self.csecret)
-		auth.set_access_token(self.atoken, self.asecret)
-
+		
+		auth = tweepy.OAuthHandler(Tweets.ckey, Tweets.csecret)
+		auth.set_access_token(Tweets.atoken, Tweets.asecret)
 		api = tweepy.API(auth)
-		results = api.user_timeline(screen_name = 'kayako', count = 20)
+		
+		#results contain all the tweets by kayako
+		results = api.user_timeline(screen_name = 'kayako', count = 17000)
 
+		#iterating through all the tweets
 		for result in results:
-			if (result._json['retweet_count'] >= 1 and "#custserv" in result._json['text']):
+			if (result._json['retweet_count'] >= 1 and "#custserv" in result._json['text']): #checking for the two conditions
 				data = {}
 				data['text'] = result._json['text']
+
+				#appending only the approved tweets to a list
 				tweet_list.append(data)
 
 		return tweet_list
